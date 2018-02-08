72b689f35b5001cd60f6de011d05db6455a6718d
Arlan <arlanvincentuy@gmail.com>
2018-02-08 23:04:59 +0800

    Fix UI for success/failure notification in contact us form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,30 +10,17 @@ class ContactsController < ApplicationController
       @contact.request = request
       if @contact.deliver
         #flash.now[:message] = nil
-        @message = "<div class='ui icon message success'>
-                    <i class='icon check'></i>
-                    <div class='content'>
-                    <div class='header'>
-                    'Message successfully sent!'
-                    </div>
-                    <p>'We will be in touch with you soon.'</p>
-                    </div>
-                    </div>"
+        @error = 0
+        @message = "Message successfully sent!"
+        @submessage = "We will be in touch with you soon"
         render :new
+
       else
         #flash.now[:message] = 'Cannot send message.'
-        @message = "<div class='ui icon message success error'>
-                    <i class='icon warning'></i>
-                    <div class='content'>
-                    <div class='header'>
-                    'Could not send your message! Please resend your e-mail.'
-                    </div>
-                    <p>'Contact HRDO if the problem still persists.'</p>
-                    </div>
-                    </div>"
+        @error = 1
+        @message = "Could not send your message! Please resend your e-mail."
+        @submessage = "Contact HRDO if the problem still persists."
         render :new
       end
-
-
     end
 end
