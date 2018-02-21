8bee7fddbb8fb7421aaf3c721e0393f65cbe76c9
Arlan <arlanvincentuy@gmail.com>
2018-02-22 00:28:34 +0800

    6th version and fixed minor issues

---
+++ app/models/contact.rb
@@ -0,0 +1,17 @@
+class Contact < MailForm::Base
+  attribute :name, :validate => true
+  attribute :employee_num, :validate => true
+  attribute :contact_num, :validate => true
+  attribute :email, :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :subject, :validate => true
+  attribute :message, :validate => true
+  attribute :nickname, :captcha => true #prevents spambots
+
+  def headers
+    {
+      :subject => "UP HRDO Contact Form",
+      :to => "gnhs.com@gmail.com",
+      :from => %("#{name}" <#{email}>)
+    }
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,42 @@
+Rails.application.routes.draw do
+
+  get 'evaluate/evaluation', to: 'evaluate#evaluation'
+
+  get 'apply/studyleave', to: 'apply#studyleave'
+
+  get 'apply/dsf', to: 'apply#dsf'
+
+  get 'apply/sabbatical', to: 'apply#sabbatical'
+
+  get 'apply/specialdetail', to: 'apply#specialdetail'
+
+  get 'apply/enrollmentprivileges', to: 'apply#enrollmentprivileges'
+
+  get 'web_pages/rsocalculator', to: 'web_pages#rsocalculator'
+
+  root 'web_pages#landingpage'
+
+  get '/landingpage', to: 'web_pages#landingpage'
+
+  get '/rsocalculator', to: 'web_pages#rsocalculator'
+
+  get 'contact/contactus',   to: 'contact#new'
+
+  get 'contact/create',    to: 'contact#create'
+
+  get 'evaluate/evaluation', to: 'evaluate#eval_SL'
+
+  get 'evaluate/evaluation', to: 'evaluate#eval_DSF'
+
+  get 'evaluate/evaluation', to: 'evaluate#eval_Sab'
+
+  get 'evaluate/evaluation', to: 'evaluate#eval_SD'
+
+  get 'evaluate/evaluation', to: 'evaluate#eval_EP'
+
+  resources :apply
+  resources :evaluate
+  resources :contact, only: [:new, :create]
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
